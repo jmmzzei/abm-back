50d84a38c26a90f83787fcff05d19adfce3f76cf
José María <josemmazzei@gmail.com>
2021-01-13 00:32:16 -0300

    Install and configure some dependencies

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+require('dotenv').config()
 const express = require('express')
+const { sequelize } = require('./models/index')
+const compression = require('compression')
+const helmet = require('helmet')
+const cors = require('cors')
 const app = express()
+
 app.set('port', 4000)
+app.use(helmet())
+app.use(compression())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 module.exports = app
