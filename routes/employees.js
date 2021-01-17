effe44bb05a17e93931c2edc91207df8690d6325
José María <josemmazzei@gmail.com>
2021-01-17 17:12:26 -0300

    Add employee middleware

--- routes/employees.js
+++ routes/employees.js
@@ -4,9 +4,9 @@ const employees = require('../controllers/employees')
 const mid = require('../middlewares/employees')
 
 router.get('/:role', mid.role, employees.getAll)
-router.get('/:role/:id', mid.role, mid.id, employees.getById)
-router.post('/', mid.post, employees.create)
-router.patch('/:id', mid.patch, employees.edit)
-router.delete('/:role/:id', mid.role, mid.id, employees.delete)
+router.get('/:role/:id', mid.roleWOAll, mid.id, employees.getById)
+router.post('/', mid.roleWOAll, mid.post, employees.create)
+router.patch('/:id', mid.roleWOAll, mid.patch, employees.edit)
+router.delete('/:role/:id', mid.roleWOAll, mid.id, employees.delete)
 
 module.exports = router
